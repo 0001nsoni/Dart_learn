e6f19e4d679b7e5932daf5adefdedd43fba5c108
Neeraj Soni <nsoni8005@gmail.com>
2025-03-02 23:44:38 +0530

    login page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,30 +10,34 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  final int days = 20;
+  final double weight = 70.2;
+  final String name = "Neeraj";
+
   @override
   Widget build(BuildContext context) {
-    int days = 20;
-    double weight = 70.2;
-    String name = "Neeraj";
-
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // home: HomePage(name: name, days: days, weight: weight), // Passing values
       themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple, 
-        appBarTheme: AppBarTheme(backgroundColor: Colors.deepPurple), // Fix AppBar color
+        primarySwatch: Colors.deepPurple,
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.deepPurple),
+        fontFamily: GoogleFonts.lato().fontFamily,
+        // primaryTextTheme: GoogleFonts.latoTextTheme()
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
-        primarySwatch: Colors.deepOrange,
-        appBarTheme: AppBarTheme(backgroundColor: Colors.deepOrange), // Fix AppBar color in dark mode
+        colorScheme: ColorScheme.dark(
+          primary: Colors.deepOrange,
+          secondary: Colors.orangeAccent,
+        ),
+        appBarTheme: const AppBarTheme(backgroundColor: Colors.deepOrange),
       ),
-      initialRoute: "/home",
+      initialRoute: "/",
       routes: {
-        "/":(context)=>LoginPage(),
-         "/home":(context)=>HomePage(name: name, days: days, weight: weight),
-        "/login":(context)=>LoginPage()
+        "/": (context) =>  LoginPage(),
+        "/home": (context) => HomePage(name: name, days: days, weight: weight),
+        "/login": (context) =>  LoginPage(),
       },
     );
   }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
-class LoginPage extends StatelessWidget {
-  
 
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Center(child:Text("Login Page",
-      style: 
-    TextStyle(fontSize: 20,
-      color: Colors.blue,fontWeight: FontWeight.bold),) ,),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/undraw_secure-login_m11a.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20.0),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 20.0),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter username",
+                    labelText: "Username",
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(height: 10.0),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Password",
+                    labelText: "Password",
+                    floatingLabelBehavior: FloatingLabelBehavior.never,
+                    border: OutlineInputBorder(),
+                  ),
+                  obscureText: true,
+                ),
+                SizedBox(height: 20.0),
+                ElevatedButton( 
+                  child:Text("login"),
+                  onPressed:(){
+                    print("Hi neeraj");
+                  },
+                  ),
+              ],
+            ),
+          ),
+          ],
+        ),
+      ),
     );
   }
 }
